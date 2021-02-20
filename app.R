2a4da4550f4df05ee76b6ebe3ed6f0b6d61007fb
Jannik Buhr <jannik.m.buhr@gmail.com>
2021-02-21 00:17:38 +0100

    add styling

--- app.R
+++ app.R
@@ -1,71 +1,72 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
+library(bslib)
 
 # Define UI for application that draws a histogram
-ui <- shinyUI(
-
-  pageWithSidebar(
-
-    headerPanel("Werwolf Online Generator")
-
-    , sidebarPanel(
+ui <- fluidPage(
+  title = "Werwolf",
+  theme = bs_theme(version = 4, bootswatch = "minty"),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+  ),
+  sidebarLayout(
+    sidebarPanel(
       selectizeInput(
-        "players"
-        , "Enter the player names"
-        , choices = NULL
-        , multiple = TRUE
-        , options = list(create = TRUE)
+        "players" ,
+        "Enter the player names" ,
+        choices = NULL ,
+        multiple = TRUE ,
+        options = list(create = TRUE)
       ),
       p("Roles have to be unique (sorry). So enter werwolf1 and werwolf2 etc. instead of werwolf twice"),
       selectizeInput(
-        "roles"
-        , "Enter the roles"
-        , choices = NULL
-        , multiple = TRUE
-        , options = list(create = TRUE)
+        "roles" ,
+        "Enter the roles" ,
+        choices = NULL ,
+        multiple = TRUE ,
+        options = list(create = TRUE)
       ),
       p("Add or remove names or roles to reshuffle")
     ),
-
     mainPanel(
-
       h4("Assignments:"),
-      tableOutput("tbl1")
-
+      p(
+        "You might want to download the table, as it will be lost when the page is reloaded",
+        class = 'note'
+      ),
+      tableOutput("tbl1"),
+      downloadButton("downloadTable",
+                     label = "download table")
     )
-
   )
 )
 
 server <- shinyServer(function(input, output) {
 
   assignment <- reactive({
-    validate(
-      need({
-        length(input$players) == length(input$roles)
-      },
-      message = "Need same number of roles as players",
-      label = "roles == players")
-    )
+    validate(need({
+      length(input$players) == length(input$roles)
+    },
+    message = "Need same number of roles as players",
+    label = "roles == players"))
 
-    df <- tibble::tibble(
-      player = input$players,
-      role = sample(input$roles)
-    )
+    df <- tibble(player = input$players,
+                 role = sample(input$roles))
     df
   })
 
   output$tbl1 <- renderTable({
     assignment()
   })
+
+  output$downloadTable <- downloadHandler(
+    filename = function() {
+      paste("werwolf-", Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
+      write_csv(assignment(), file)
+    }
+  )
 
 })
 
